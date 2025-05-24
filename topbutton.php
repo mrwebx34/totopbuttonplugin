a6990cee521e5658702ee646c3093714e5ca8be7
mrwebx34 <84919416+mrwebx34@users.noreply.github.com>
2025-05-24 14:06:48 +0530

    Initial commit

---
+++ topbutton.php
@@ -0,0 +1,15 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/*
+ * Plugin Name:       WP Simple
+ * Description:       A simple  Top Button.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ranjan
+ * License:           GPL v2 or later
+ */
+?>
